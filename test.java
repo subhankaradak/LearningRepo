d8079872dc6d1bdae76660d27b22e3154df3c029
subhankaradak <30392383+subhankaradak@users.noreply.github.com>
2018-03-10 21:08:06 +0530

    test.java 2nd version commoit
    test.java 2nd version commoit

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ public class test
 {
   public void static main(String args[])
   {
-    System.out.println("Hii , this is the first version 1.0");
+    System.out.println("Hii , this is the 2nd version 2.0");
   }
 }
